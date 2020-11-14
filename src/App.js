54ac9be1a99cb1c3d6bce00ff92061c86c0fee21
Ramadan777 <faysol@mail.ru>
2020-11-14 15:07:06 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+import './style.css';
+import { useDispatch, useSelector } from 'react-redux'
 
 function App() {
+  const title = useSelector(state => state.title);
+  const message = useSelector(state => state.message);
+  const dispatch = useDispatch();
+
+  const remove = (id) => {
+    dispatch({type: 'setDelete', payload: id});
+  }
+
+  const toggleStar = (id) => {
+    dispatch({type: 'tsShow', payload: id});
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        {title}
+      </div>
+      <div className="input">
+        <input type="text" placeholder="Введите текст.." />
+        <button className="">
+          Добавить
+        </button>
+      </div>
+        <div className="main">
+          {message.map((t) => {
+            return (
+              <div className={`content ${t.favorite ? 'selected': ''}`}>
+                <div className="star" onClick={() => toggleStar(t.id)}>
+                  <span>⭑</span>
+                </div>
+                <div className="text">
+                  {t.text}
+                </div>
+                <button className="btn" onClick={() => remove(t.id)}>
+                  X
+                </button>
+              </div>
+            )
+          })}
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+
+const todos = {
+  title: 'Список дел',
+  message: [
+    {id: 1, text: 'Купить бананы', favorite: false },
+    {id: 2, text: 'Продать квартиру', favorite: false },
+    {id: 3, text: 'Выучить уроки по JavaScript', favorite: true }
+  ]
+}
+
+function reducer(state = todos, action) {
+  switch(action.type) {
+    case 'setDelete':
+      return {
+        ...state,
+        message: state.message.filter(message => message.id !== action.payload)
+      }
+
+    case 'tsShow':
+      return {
+        ...state,
+        message: state.message.map((item) => {
+          if(item.id === action.payload) {
+            return {
+              ...item,
+              favorite: !item.favorite
+            }
+          }
+
+          return item;
+        })
+      }
+
+    default:
+      return state;
+
+  }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
